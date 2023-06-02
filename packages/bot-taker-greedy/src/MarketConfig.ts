ebdbaac0a42b85b5acbb7903450e9272efb138a3
Espen Højsgaard <espen@espen.dk>
2023-06-02 08:36:16 +0200

    feat(greedy bot): Allow use of alternative token symbol for price lookup (#29)

--- packages/bot-taker-greedy/src/MarketConfig.ts
+++ packages/bot-taker-greedy/src/MarketConfig.ts
@@ -5,6 +5,8 @@ export type TakerConfig = {
 
 export type MarketConfig = {
   baseToken: string;
+  baseTokenSymbolForPriceLookup?: string; // Allow use of another symbol when looking up a price an unlisted token (eg MATIC instead for WMATIC)
   quoteToken: string;
+  quoteTokenSymbolForPriceLookup?: string; // Same as baseTokenSymbolForPriceLookup
   takerConfig: TakerConfig;
 };
