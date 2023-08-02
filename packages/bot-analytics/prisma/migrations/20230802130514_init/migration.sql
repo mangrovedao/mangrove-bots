1569b1f319fbe53365584c73a41c6d26dc3ec475
Louis-Amas <amaslouis@protonmail.com>
2023-08-02 15:12:49 +0200

    feat: bot-analytics: initial commit

---
+++ packages/bot-analytics/prisma/migrations/20230802130514_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Chain" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Chain_pkey" PRIMARY KEY ("id")
+);
---
+++ packages/bot-analytics/src/index.ts
@@ -0,0 +1,49 @@
+/**
+ * A simple configurable gas price update bot for the Mangrove DEX.
+ * @module
+ */
+
+import config from "./util/config";
+import { logger } from "./util/logger";
+
+import { BaseProvider } from "@ethersproject/providers";
+import { Wallet } from "@ethersproject/wallet";
+import Mangrove, { enableLogging } from "@mangrovedao/mangrove.js";
+
+import { AsyncTask, SimpleIntervalJob, ToadScheduler } from "toad-scheduler";
+import { ExitCode, Setup } from "@mangrovedao/bot-utils";
+
+import { PrismaClient } from "@prisma/client";
+
+enableLogging();
+
+const scheduler = new ToadScheduler();
+const setup = new Setup(config);
+
+const primsa = new PrismaClient();
+
+async function botFunction(
+  mgv: Mangrove,
+  signer: Wallet,
+  provider: BaseProvider
+) {}
+
+const main = async () => {
+  const chains = await primsa.chain.findFirst();
+  console.log(chains);
+
+  // await primsa.chain.create({
+  //   data: {
+  //     id: 1,
+  //     name: 'Polygon'
+  //   }
+  // });
+
+  // await setup.startBot("update gas bot", botFunction, scheduler);
+};
+
+main().catch(async (e) => {
+  await primsa.$disconnect();
+  logger.error(e);
+  setup.stopAndExit(ExitCode.ExceptionInMain, scheduler);
+});
